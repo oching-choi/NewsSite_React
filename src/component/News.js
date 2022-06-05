856359be6189d6606de2c0dcfdd487bd29516b35
oching-choi <oching.choi@gmail.com>
2022-06-05 15:57:27 +0900

    feat : News API로 기사 검색 , 출력 기능 추가 axios, async/await 로 기능구현.

--- src/component/News.js
+++ src/component/News.js
@@ -1,24 +1,75 @@
+import{ useState } from 'react';
+import axios from 'axios';
+
 function News(props){
-    return(
+    const [topic, setTopic] = useState('');
+
+    return( 
       <div className='home-wrapper'>
         <h1 className='logo'>NewsSite</h1>
         <form className='search-form'>
-            <input type='text' className='search-input' placeholder='검색어를 입력해주세요.'></input>
+            <input type='text' className='search-input' placeholder='검색어를 입력해주세요.' onChange={(e)=>{
+              setTopic(e.target.value);
+            }}></input>
             <input type='submit' className='submit-btn' value='검색' onClick={(e)=>{
               getNews(e);
             }}></input>
         </form>
         <div className='news-area'>
           <ul className='news'>
-            <li className='news-list'>어쩌구</li>
+           
           </ul>
         </div>
       </div>
     )
+  
+  async function getNews(e) {
+    const apiKey = '2721d1f0de38415b978ddeed5ff2291a';
+    let url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`;
+
+    e.preventDefault();
+
+    try {
+      const response = await axios.get(url);
+      const article = response.data.articles;
+      //news list 렌더함수
+      renderNews(article);
+    } catch (error) {
+      //error list 렌더
+      renderError()
+    }
   }
 
-  async function getNews(e){
-    e.preventDefault()
+  function renderNews(article){
+    const news = document.querySelector('.news');
+    let newsList = article.reduce((prevLi, currentLi, index)=>{
+        return (
+            prevLi + 
+            `<li class="news-list">
+               <a href="${article[index].url}" target="_blank">
+                    <div class="news-thumbnail">
+                        <img src="${article[index].urlToImage}">
+                    </div>
+                    <div class="news-contents">
+                        <div class="news-info">
+                            <span class="news-author">${article[index].author}</span>
+                            <span class="news-date">${article[index].publishedAt}</span>
+                        </div>
+                        <p class="news-title">${article[index].title}</p>
+                        <p class="news-description">${article[index].description}</p>
+                    </div>
+               </a> 
+            </li>`
+        )
+    },'');
+    news.innerHTML=newsList;
   }
+
+  function renderError(){
+    const news = document.querySelector('.news');
+    let newsList = `<li style="color:red">error<li>`
+    news.innerHTML=newsList;
+  }
+}
 
   export default News
